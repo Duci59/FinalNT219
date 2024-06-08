d8ada9ecf3814f406630520bd05aeb09f5810d2b
nguyenchien1101 <vonguyenchien1101@gmail.com>
2024-06-08 19:10:54 +0700

    delete

--- MusicApp/Forms/MainMenu.cs
+++ MusicApp/Forms/MainMenu.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FireSharp.Interfaces;
+using FireSharp.Response;
+using MusicApp.env;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,6 +16,7 @@ namespace MusicApp.Forms
     public partial class MainMenu : Form
     {
         string Username, Usertype;
+        private readonly Service firebaseService;
         public MainMenu(string username, string usertype)
         {
             InitializeComponent();
@@ -44,21 +48,16 @@ namespace MusicApp.Forms
         {
             this.WindowState |= FormWindowState.Minimized;
         }
-        string[] activities = { "Lối Nhỏ", "Một triệu like", "Đi Về Nhà" };
-        string[] auth = { "Đen Vâu", "Đen", "Vâu " };
-        string[] time = { "3:32", "3:42", "4:00" };
+      
         private void MainMenu_Load(object sender, EventArgs e)
         {
-            int y = 0; // Đặt vị trí y ban đầu là 0
-            for (int i = 0; i < 3; i++)
-            {
-                CustomPanel pn = new CustomPanel(activities[i], auth[i], time[i])
-                {
-                    Location = new Point(0, y), // Đặt vị trí của CustomPanel theo vị trí y hiện tại
-                };
-                y += pn.Height + 10; // Tăng vị trí y để các CustomPanel được đặt cách nhau 10 pixel dọc
-                panel6.Controls.Add(pn);
-            }
+           
+        }
+
+        private async void btnPlayMusic_Click(object sender, EventArgs e)
+        {
+            
+
         }
 
         private void btnUploadFiles_Click(object sender, EventArgs e)
